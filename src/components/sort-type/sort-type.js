1292d13b41e9da708abae2c643a094bd0135731b
nogandalfgrey <no.gandalf.grey@gmail.com>
2019-08-28 15:38:24 +0300

    Styled sort type component

--- src/components/sort-type/sort-type.js
+++ src/components/sort-type/sort-type.js
@@ -1,30 +1,78 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import { changeSortType } from './../../actions';
 
+const StyledWrapper = styled.div`
+  display: flex;
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  text-transform: uppercase;
+  color: #FFFFFF;overflow: hidden;
+  
+  
+  .visually-hidden {
+    position: absolute;
+    clip: rect(0 0 0 0);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+  }
+`;
+
+const StyledLabel = styled.label`
+  cursor: pointer;
+  
+  :first-child .input-text {
+    border-radius: 8px 0 0 8px;
+  }
+  :last-child .input-text {
+    border-radius: 0 8px 8px 0;
+  }
+`;
+
+const InputText = styled.div`
+  padding: 15px 57px;
+  border: 1px solid #DFE5EC;
+  background-color: #FFFFFF;
+  color: #4A4A4A;
+  
+  input:checked + & {
+    background-color: #2196F3;
+    color: #FFFFFF;
+  }
+`;
+
 const SortType = ({ sortType, sortTypeHandler }) => {
+  const SortTypeItem = ({ dataSort, children, checked }) => {
+    return (
+      <StyledLabel>
+        <input
+            name='sort'
+            type='radio'
+            data-sort={dataSort}
+            checked={checked}
+            onChange={(e) => sortTypeHandler(e)}
+            className='visually-hidden'/>
+        <InputText className='input-text'>{children}</InputText>
+      </StyledLabel>
+    );
+  };
+
   return (
-    <div>
-      <label>
-        <input
-          name='sort'
-          type='radio'
-          data-sort='cheap'
-          checked={ sortType.cheap }
-          onChange={ (e) => sortTypeHandler(e) }/>
+    <StyledWrapper>
+      <SortTypeItem dataSort={'cheap'} checked={sortType.cheap}>
         Самый дешевый
-      </label>
-      <label>
-        <input
-          name='sort'
-          type='radio'
-          data-sort='fast'
-          checked={ sortType.fast }
-          onChange={ (e) => sortTypeHandler(e) }/>
+      </SortTypeItem>
+      <SortTypeItem dataSort={'fast'} checked={sortType.fast}>
         Самый быстрый
-      </label>
-    </div>  
+      </SortTypeItem>
+    </StyledWrapper>
   )
 };
 
