4711da62b66cd6a5e9705b061306bc97bc19a665
nogandalfgrey <no.gandalf.grey@gmail.com>
2019-08-31 12:12:35 +0300

    Updates

---
+++ precache-manifest.404f1a3a0f74282315c4129e2b2b491b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30deb893c715b6f0973af32bab8d07d",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "e8753fd9fe118a9528cc",
+    "url": "/aviasales/static/js/2.35cf7706.chunk.js"
+  },
+  {
+    "revision": "99f90d058ea528999cf6",
+    "url": "/aviasales/static/js/main.68784d08.chunk.js"
+  },
+  {
+    "revision": "bfb47426b928d72b1ef0",
+    "url": "/aviasales/static/js/runtime~main.726f6c9b.js"
+  },
+  {
+    "revision": "fc4db7ee86a2b160e9520aee39d5338f",
+    "url": "/aviasales/static/media/checkbox-active.fc4db7ee.svg"
+  },
+  {
+    "revision": "ebdd84bf07785b90e6f27333b09592c2",
+    "url": "/aviasales/static/media/checkbox.ebdd84bf.svg"
+  },
+  {
+    "revision": "96990a1b3638b68f706e0a3331be2821",
+    "url": "/aviasales/static/media/logo.96990a1b.svg"
+  }
+]);
