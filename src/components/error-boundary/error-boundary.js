6b4e183776425d4a49637a5da05af1e768b362ff
nogandalfgrey <no.gandalf.grey@gmail.com>
2019-08-31 12:00:30 +0300

    Error boundary component

--- src/components/error-boundary/error-boundary.js
+++ src/components/error-boundary/error-boundary.js
@@ -1,19 +1,47 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
 
-import ErrorIndicator from '../error-indicator';
+const StyledError = styled.div`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  padding: 10px;
+  background-color: tomato;
+  border-radius: 0 10px 0 0;
+  
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  color: #FFFFFF;
+`;
 
-class ErrorBoundary extends Component {
-  render() {
-    if (this.props.error) {
-      return <ErrorIndicator />;
-    }
-    return this.props.children;
-  }
-}
+const StyledLink = styled.a`
+  color: #FFFFFF;
+`;
 
-const mapStateToProps = ({ error }) => {
-  return { error };
+const ErrorIndicator = ({ requestData }) => {
+  return (
+    <StyledError>
+      Ошибка: не все билеты загружены. <StyledLink href='request' onClick={(e) => requestData(e)}>Попробуйте снова</StyledLink>
+    </StyledError>
+  )
 };
 
-export default connect(mapStateToProps)(ErrorBoundary);
+const ErrorBoundary = ({ error, children, requestFunc }) => {
+  const requestData = (e) => {
+    e.preventDefault();
+    requestFunc()
+  };
+
+  return (
+    <Fragment>
+      {children}
+      {error ? <ErrorIndicator requestData={requestData}/> : null}
+    </Fragment>
+  )
+};
+
+export default ErrorBoundary;
--- src/components/error-indicator/error-indicator.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ErrorIndicator = () => {
-  return <div className="error-indicator">Error</div>;
-};
-
-export default ErrorIndicator;
